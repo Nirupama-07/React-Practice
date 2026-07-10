9a1b8de07346aad15e363853e5a35caab7008595
Nirupama <nirupamamajhi549@gmail.com>
2026-07-11 05:14:38 +0530

    Added chapter-1

---
+++ Chapter-1-Introduction to React/src/App.jsx
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <>
+      <div>
+        <h1>Welcome to React!</h1>
+        <h2>My Name:Nirupama Majhi</h2>
+      </div>
+      <div>
+        <p>This is my first react application</p>
+        <h4>React development server is running successfully</h4>
+      </div>
+    </>
+  )
+}
+
+export default App
